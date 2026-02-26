78db208a934b2bb87e7b081b313aa7c9bc724318
SophieKiesbye <sophiekholm@live.dk>
2026-02-26 13:34:01 +0100

    .

--- web-api/obj/Debug/net9.0/web-api.AssemblyInfo.cs
+++ web-api/obj/Debug/net9.0/web-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12514956a4a3a2bc0b91db919da455ae55c3e404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e03dbc48ecbeaf891df09374fe017fd362c2be")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
